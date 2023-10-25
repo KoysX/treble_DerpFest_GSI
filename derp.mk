facd65811f474c4338cfa2235bcfd0f91bdd07e3
KoysX <2685697179b@gmail.com>
2023-10-25 11:08:45 -0400

    Sync with latest TrebleDroid source

--- derp.mk
+++ derp.mk
@@ -8,16 +8,13 @@ $(call inherit-product, device/derp/sepolicy/common/sepolicy.mk)
 TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
 TARGET_USES_PREBUILT_VENDOR_SEPOLICY := true
 SELINUX_IGNORE_NEVERALLOWS := true
-BOARD_EXT4_SHARE_DUP_BLOCKS := true
+#BOARD_EXT4_SHARE_DUP_BLOCKS := true
 BUILD_BROKEN_DUP_RULES := true
 
 TARGET_NO_KERNEL_OVERRIDE := true
 TARGET_NO_KERNEL_IMAGE := true
 
-TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_BOOT_ANIMATION_RES := 1080
-EXTRA_UDFPS_ANIMATIONS := true
-USE_LEGACY_BOOTANIMATION := false
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.system.ota.json_url=https://raw.githubusercontent.com/KoysX/treble_DerpFest_GSI/13/ota.json
